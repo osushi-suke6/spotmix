889091d44e381b2f88a2f81f9e2b0c11c773b74b
Sukeroku <osushi.suke6@gmail.com>
2023-04-06 00:01:57 +0900

    update: RootPage

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,11 +1,8 @@
-import TokenValidaterContainer from '../organisms/containers/TokenValidaterContainer';
-
 export default function RootPage() {
   return (
     <>
       <h1>Root</h1>
       <p>access token check</p>
-      <TokenValidaterContainer />
     </>
   );
 }
