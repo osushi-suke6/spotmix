4379b200ed0e9bdd6f2f44c22d56e3fc7dc0de0e
Sukeroku <osushi.suke6@gmail.com>
2023-04-09 00:38:59 +0900

    refactor: useScrollBottom hook

--- src/hooks/useScrollBottom.tsx
+++ src/hooks/useScrollBottom.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 const useScrollBottom = (callback: () => void) => {
   const divRef = useRef<HTMLDivElement>(null);
 
+  const handleScroll = useCallback(() => {
+    if (!divRef.current) return;
+
+    const { scrollTop, scrollHeight, clientHeight } = divRef.current;
+    const isBottom = scrollTop + clientHeight === scrollHeight;
+
+    if (isBottom) callback();
+  }, [callback]);
+
   useEffect(() => {
-    const handleScroll = () => {
-      if (!divRef.current) return;
-
-      const { scrollTop, scrollHeight, clientHeight } = divRef.current;
-      const isBottom = scrollTop + clientHeight === scrollHeight;
-
-      if (isBottom) callback();
-    };
-
     divRef.current?.addEventListener('scroll', handleScroll);
 
     return () => {
-      removeEventListener('scroll', handleScroll);
+      divRef.current?.removeEventListener('scroll', handleScroll);
     };
-  }, [callback]);
+  }, [handleScroll]);
 
   return divRef;
 };
