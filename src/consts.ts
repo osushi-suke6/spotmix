3d0adc40f9361716ee3af59410e48f3042da309e
Sukeroku <osushi.suke6@gmail.com>
2023-04-11 00:49:03 +0900

    add: useSpotifyToken

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+const ACCESS_KEY = 'access-token';
+const CLIENT_ID = '45841e6544c0477a983f3f89cfd22e26';
+const REFRESH_KEY = 'refresh-token';
+
+export { ACCESS_KEY, CLIENT_ID, REFRESH_KEY };
---
+++ src/hooks/useSpotifyToken.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { ACCESS_KEY, CLIENT_ID, REFRESH_KEY } from '../consts';
+
+interface IResult {
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+  token_type: string;
+}
+
+const URL = 'https://accounts.spotify.com/api/token';
+
+const useSpotifyToken = (refreshToken: string) => {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem(ACCESS_KEY, token);
+  }, [token]);
+
+  useEffect(() => {
+    localStorage.setItem(REFRESH_KEY, refreshToken);
+  }, []);
+
+  const refresh = useCallback(async () => {
+    const body = new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: localStorage.getItem(REFRESH_KEY) ?? '',
+      client_id: CLIENT_ID,
+    });
+
+    const args = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body,
+    };
+
+    const res = await fetch(URL, args);
+
+    if (!res.ok) throw Error('Refresh Failed');
+
+    const json = (await res.json()) as IResult;
+
+    localStorage.setItem(REFRESH_KEY, json.refresh_token);
+    setToken(json.access_token);
+  }, []);
+
+  return [token, refresh] as const;
+};
+
+export default useSpotifyToken;
